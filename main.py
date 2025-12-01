fc55693b731871b5b71b10efe6588ccd413ad8ed
Eliot Pazzé <eliot.pazze@univ-tlse3.fr>
2025-12-01 15:27:22 +0100

    Using multiprocessing for multitasking in python

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from tp-multithreading!")
+from multiprocessing import Process, Manager
+from task import Task
+
+
+def worker(queue):
+    while not queue.empty():
+        task = queue.get()
+        result = task.work()
+        print(f"Résultat : {result}")
 
 
 if __name__ == "__main__":
-    main()
+    manager = Manager()
+    task_queue = manager.Queue()
+    # Remplir avec des objets Task
+    for i in range(10):
+        t = Task(i, 100)
+        task_queue.put(t)
+    # Démarrer les Minions
+    processes = [Process(target=worker, args=(task_queue,)) for _ in range(4)]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
